4a091a3fb5eb8546138cad26004c42d7178813b9
Yuichi Tateno <hotchpotch@gmail.com>
2024-09-22 15:32:43 +0900

    Add splade embedder

---
+++ src/jmteb/embedders/splade_embedder.py
@@ -0,0 +1,74 @@
+from __future__ import annotations
+
+import numpy as np
+from yasem import SpladeEmbedder as YasemSpladeEmbedder
+
+from jmteb.embedders.base import TextEmbedder
+
+
+class SpladeEmbedder(TextEmbedder):
+    """Splade embedder."""
+
+    def __init__(
+        self,
+        model_name_or_path: str,
+        batch_size: int = 32,
+        device: str | None = None,
+        max_seq_length: int | None = 512,
+        add_eos: bool = False,
+        model_kwargs: dict | None = None,
+        tokenizer_kwargs: dict | None = None,
+    ) -> None:
+        model_kwargs = self._model_kwargs_parser(model_kwargs)
+        self.model = YasemSpladeEmbedder(
+            model_name_or_path,
+            trust_remote_code=True,
+            model_kwargs=model_kwargs,
+            tokenizer_kwargs=tokenizer_kwargs,
+            max_seq_length=max_seq_length,
+            device=device,  # type: ignore
+        )
+        if max_seq_length:
+            self.model.max_seq_length = max_seq_length
+
+        self.batch_size = batch_size
+        self.device = device
+        self.max_seq_length = self.model.max_seq_length
+        self.add_eos = add_eos
+
+        self.set_output_numpy()
+
+    def encode(
+        self,
+        text: str | list[str],
+        show_progress_bar: bool = True,
+    ) -> np.ndarray:
+        if self.add_eos:
+            text = self._add_eos_func(text)
+        is_single_text = isinstance(text, str)
+        if is_single_text:
+            text = [text]
+        embeddings = self.model.encode(
+            text,
+            convert_to_numpy=True,
+            batch_size=self.batch_size,
+            device=self.device,  # type: ignore
+            show_progress_bar=show_progress_bar,
+        )
+        if is_single_text:
+            embeddings = embeddings[0]
+        return embeddings  # type: ignore
+
+    def _add_eos_func(self, text: str | list[str]) -> str | list[str]:
+        try:
+            eos_token = getattr(self.model.tokenizer, "eos_token")
+        except AttributeError:
+            return text
+
+        if isinstance(text, str):
+            return text + eos_token
+        elif isinstance(text, list):
+            return [t + eos_token for t in text]
+
+    def get_output_dim(self) -> int:
+        return self.model.vocab_size
